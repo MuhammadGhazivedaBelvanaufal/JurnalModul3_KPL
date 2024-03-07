91297e9805554ee1275a0f51befe0b66a12b9e98
unknown <qhikizaalicia@gmail.com>
2024-03-07 10:13:37 +0700

    Commit Pertama

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JurnalMod3_1302220011
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
